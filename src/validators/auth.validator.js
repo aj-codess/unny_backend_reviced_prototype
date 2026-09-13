aa346c7a69c4fd4213210e4372f6b935a1927d11
aj-codess <jagyei339@gmail.com>
2026-09-13 17:48:07 -0500

    finished product

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -32,7 +32,7 @@ export const registerValidator = [
 ];
 
 export const loginValidator = [
-  body('email').isEmail().withMessage('Valid email is required').normalizeEmail(),
+  body('email').isEmail().withMessage('Valid email is required'),
   body('password').notEmpty().withMessage('Password is required'),
 ];
 
